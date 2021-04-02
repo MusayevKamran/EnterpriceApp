307ec2377cfbd25a8e553582e113d911ec11593b
Kamran <kamranmusayev90@gmail.com>
2021-04-02 12:52:28 +0400

    Remove blog from Model

---
+++ App.Infrastructure.CrossCutting.Identity/Data/Migrations/20210402084902_InitialIdentity.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace App.Infrastructure.CrossCutting.Identity.Migrations
+{
+    public partial class InitialIdentity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
